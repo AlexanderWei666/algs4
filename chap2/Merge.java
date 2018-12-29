6a95321c45a33fdab8a5b538e50341363fc45631
AlexanderWei <alexanderwei0013@gmail.com>
2018-12-29 22:21:03 +0800

    测试算法

--- chap2/Merge.java
+++ chap2/Merge.java
@@ -1,6 +1,29 @@
 public class Merge {
     private static Comparable[] aux;
 
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++){
+            if(less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+
     public static void sort(Comparable[] a){
         aux = new Comparable[a.length];
         sort(a, 0, a.length - 1);
@@ -30,6 +53,13 @@ public class Merge {
             else if (less(aux[j], aux[i])) a[k] = aux[j++];//if aux[i] > aux[j] merge first of the right array
             else a[k] = aux[i++];//if aux[i] < aux[j] merge first of the left array
         }
+    }
+
+    public static void main(String[] args) {
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
     }
 
 }
--- chap2/MergeBU.java
+++ chap2/MergeBU.java
@@ -1,5 +1,28 @@
 public class MergeBU {
-    private Comparable[] aux;
+    private static Comparable[] aux;
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++){
+            if(less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
 
     public static void sort(Comparable[] a) {
         int N = a.length;
@@ -23,5 +46,12 @@ public class MergeBU {
             else if (less(aux[j], aux[i])) a[k] = aux[j++];//if aux[i] > aux[j] merge first of the right array
             else a[k] = aux[i++];//if aux[i] < aux[j] merge first of the left array
         }
+    }
+
+     public static void main(String[] args) {
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
     }
 }
